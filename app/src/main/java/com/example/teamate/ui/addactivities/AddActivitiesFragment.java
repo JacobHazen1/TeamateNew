281fee35317ee6979ca88ae36d34e8ec44cd1f6b
jacob.hazen <jacob.hazen@ucalgary.ca>
2019-11-28 21:35:21 -0700

    Touching up leaderboard and profile

--- app/src/main/java/com/example/teamate/ui/addactivities/AddActivitiesFragment.java
+++ app/src/main/java/com/example/teamate/ui/addactivities/AddActivitiesFragment.java
@@ -1,30 +1,29 @@
-package com.example.teamate.ui.settings;
+package com.example.teamate.ui.addactivities;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.teamate.R;
 
-public class SettingsFragment extends Fragment {
+public class AddActivitiesFragment extends Fragment {
 
-    private settingsViewModel settingsViewModel;
+    private NotificationsViewModel notificationsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        settingsViewModel =
-                ViewModelProviders.of(this).get(settingsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_settings, container, false);
-//        final TextView textView = root.findViewById(R.id.text_settings);
-//        settingsViewModel.getText().observe(this, new Observer<String>() {
+        notificationsViewModel =
+                ViewModelProviders.of(this).get(NotificationsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_addactivities, container, false);
+
+
+//        final TextView textView = root.findViewById(R.id.text_notifications);
+//        notificationsViewModel.getText().observe(this, new Observer<String>() {
 //            @Override
 //            public void onChanged(@Nullable String s) {
 //                textView.setText(s);
@@ -32,4 +31,5 @@ public class SettingsFragment extends Fragment {
 //        });
         return root;
     }
+
 }
