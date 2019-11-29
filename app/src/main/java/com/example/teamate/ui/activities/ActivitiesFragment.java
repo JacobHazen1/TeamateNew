cdd1aaba3823ccffefdab3fe8a5f96117ec5b4d5
jacob.hazen <jacob.hazen@ucalgary.ca>
2019-11-28 19:58:22 -0700

    ICONS DONE

--- app/src/main/java/com/example/teamate/ui/activities/ActivitiesFragment.java
+++ app/src/main/java/com/example/teamate/ui/activities/ActivitiesFragment.java
@@ -1,33 +1,43 @@
 package com.example.teamate.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
+import com.example.teamate.LoginActivity;
 import com.example.teamate.R;
 
 public class ActivitiesFragment extends Fragment {
+    private TextView hryoga;
 
-//    private ActivitiesViewModel activitiesViewModel;
+
+
+    //    private ActivitiesViewModel activitiesViewModel;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         super.onCreateView(inflater, container, savedInstanceState);
-        final View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final View root = inflater.inflate(R.layout.fragment_activities, container, false);
         ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
+        hryoga = (TextView) root.findViewById(R.id.HR_Yoga);
+        hryoga.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ActivitiesFragment.this.getActivity(), LoginActivity.class));
+            }
+        });
+
 //        return rootview;
 //        activitiesViewModel =
 //                ViewModelProviders.of(this).get(ActivitiesViewModel.class);
-//        View root = inflater.inflate(R.layout.fragment_home, container, false);
+//        View root = inflater.inflate(R.layout.fragment_activities, container, false);
 //        final TextView textView = root.findViewById(R.id.text_home);
 //        activitiesViewModel.getText().observe(this, new Observer<String>() {
 //            @Override
