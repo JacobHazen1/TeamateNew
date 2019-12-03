d066656904464ee080d4b14570ea3a1cf28d296a
Jose Orellana <jose98orellana@users.noreply.github.com>
2019-12-02 18:48:40 -0700

    Batch #1
    Changes made to database handler

--- app/src/main/java/com/example/teamate/data/myDBHandler.java
+++ app/src/main/java/com/example/teamate/data/myDBHandler.java
@@ -9,16 +9,21 @@ import android.database.Cursor;
 public class MyDBHandler extends SQLiteOpenHelper {
     //information of database
     private static final int DATABASE_VERSION = 1;
-    private static final String DATABASE_NAME = "studentDB.db";
-    public static final String TABLE_NAME = "Student";
-    public static final String COLUMN_ID = "StudentID";
-    public static final String COLUMN_NAME = "StudentName";
+    private static final String ACCOUNT_DATABASE = "accountDB.db";
+    public static final String TABLE_NAME = "Account";
+    public static final String COLUMN_FNAME = "FirstName";
+    public static final String COLUMN_LNAME = "LastName";
+    public static final String COLUMN_EMAIL = "Email";
+    public static final String COLUMN_UNAME = "UserName";
     //initialize the database
     public MyDBHandler(Context context, Stringname, SQLiteDatabase.CursorFactoryfactory, intversion) {
-        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+        super(context, ACCOUNT_DATABASE, factory, DATABASE_VERSION);
     }
     @Override
-    public void onCreate(SQLiteDatabase db) {}
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_TABLE = "CREATE TABLE" + TABLE_NAME + "(" + COLUMN_FNAME + "INTEGER PRIMARYKEY," + COLUMN_LNAME + "TEXT," + COLUMN_EMAIL + "TEXT," + COLUMN_UNAME + "TEXT )";
+        db.execSQL(CREATE_TABLE);
+    }
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {}
     public String loadHandler() {}
